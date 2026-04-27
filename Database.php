647f3882b86717301663237e464bab47eab0c7fa
Eslam Saeed <eslamsaeed298@gmail.com>
2026-04-28 00:47:23 +0300

    Enhance Database query method to accept parameters for prepared statements

--- Database.php
+++ Database.php
@@ -28,13 +28,19 @@ class Database {
      * Query the database
      * 
      * @param string query
+     * @param array params
      * 
      * @return PDOStatement
      * @throws PDOException
      */
-    public function query($query) {
+    public function query($query, $params = []) {
         try {
             $stmt = $this->conn->prepare($query);
+            
+            foreach($params as $param => $value) {
+                $stmt->bindValue(":{$param}", $value);
+            }
+                
             $stmt->execute();
             
             return $stmt;
