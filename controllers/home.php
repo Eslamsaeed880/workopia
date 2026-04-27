01a379593bf5242fa4ac96be75d28e6efe3f61ef
Eslam Saeed <eslamsaeed298@gmail.com>
2026-04-28 00:06:53 +0300

    Enhance Router class with route registration documentation; update home controller to fetch and display job listings dynamically

--- controllers/home.php
+++ controllers/home.php
@@ -1,4 +1,11 @@
 <?php
-require_once '../helpers.php';
 
-loadView('home');
+$config = require basePath('config/db.php');
+$db = new Database($config);
+
+$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+
+loadView('home', [
+    'listings' => $listings,
+]);
--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
 <?php
 require_once '../helpers.php';
-
-
 require_once basePath('Router.php');
+require_once basePath('Database.php');
 
+// Instantiating the router
 $router = new Router();
 
+// Get routes
 $routes = require basePath('routes.php');
 
-
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
