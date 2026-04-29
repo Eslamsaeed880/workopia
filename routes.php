ce97b2a3888d0fb6eb8ee894c6b4c2068d594a47
Eslam Saeed <eslamsaeed298@gmail.com>
2026-04-29 04:29:35 +0300

    Implement create and show methods in ListingController; update routes for listings

--- routes.php
+++ routes.php
@@ -2,6 +2,5 @@
 
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
-// $router->get('/', 'HomeController@index');
-// $router->get('/listings/create', 'ListingController@create');
-// $router->get('/listing', 'ListingController@show');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
