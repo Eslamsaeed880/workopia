17c72098910947e53e5cf1dc846c698bce894eaa
Eslam Saeed <eslamsaeed298@gmail.com>
2026-05-01 04:44:22 +0300

    Refactor error message handling; create reusable error partial and update views to utilize it

--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -5,13 +5,15 @@
 <div class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-500 mx-6">
     <h2 class="text-4xl text-center font-bold mb-4">Register</h2>
-    <form>
+    <?= loadPartial('errors', ['errors' => $errors ?? []]) ?>
+    <form method="POST" action="/auth/register">
         <div class="mb-4">
         <input
             type="text"
             name="name"
             placeholder="Full Name"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['name'] ?? '' ?>"
         />
         </div>
         <div class="mb-4">
@@ -20,6 +22,7 @@
             name="email"
             placeholder="Email Address"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['email'] ?? '' ?>"
         />
         </div>
         <div class="mb-4">
@@ -28,6 +31,7 @@
             name="city"
             placeholder="City"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['city'] ?? '' ?>"
         />
         </div>
         <div class="mb-4">
@@ -36,6 +40,7 @@
             name="state"
             placeholder="State"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['state'] ?? '' ?>"
         />
         </div>
         <div class="mb-4">
@@ -44,6 +49,7 @@
             name="password"
             placeholder="Password"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['password'] ?? '' ?>"
         />
         </div>
         <div class="mb-4">
@@ -52,6 +58,7 @@
             name="password_confirmation"
             placeholder="Confirm Password"
             class="w-full px-4 py-2 border rounded focus:outline-none"
+            value="<?= $user['password_confirmation'] ?? '' ?>"
         />
         </div>
         <button
