b119f7b34b091c4c01b4761c307880a5685a0114
Eslam Saeed <eslamsaeed298@gmail.com>
2026-05-01 05:00:07 +0300

    Implement session management; add Session class with methods for starting, setting, getting, checking, and clearing session data; update index.php to utilize the Session class.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
-session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
-require_once '../helpers.php';
 
 use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+require_once '../helpers.php';
 
 // Instantiating the router
 $router = new Router();
